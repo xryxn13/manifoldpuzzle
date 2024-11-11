f9178e9d71054be5b0dfcebf69a3fa02610a2b8c
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-11-11 19:08:23 +0530

    Update square_manifold_maker_streamlit.py

--- square_manifold_maker_streamlit.py
+++ square_manifold_maker_streamlit.py
@@ -220,8 +220,8 @@ if image1_file and image2_file:
         )
 
 zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, "w") as zipf:
-            for i, (layout_name, layout) in enumerate(layouts.items()):
+with zipfile.ZipFile(zip_buffer, "w") as zipf:
+    for i, (layout_name, layout) in enumerate(layouts.items()):
                 grid_image = create_grid(image1_parts, image2_parts, other_image, layout)
                 img_bytes = io.BytesIO()
                 grid_image.save(img_bytes, format="PNG")
